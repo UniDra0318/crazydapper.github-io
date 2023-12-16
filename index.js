4c9aaec58df15f25a28af918c14f06bee77e83d7
UniDra0318 <153459470+UniDra0318@users.noreply.github.com>
2023-12-17 01:55:51 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var navLinks = document.getElementById("navLinks")
+
+        function hideMenu(){
+            navLinks.style.marginRight = "0"
+            navLinks.style.width = "0"
+        }
+        function showMenu(){
+            navLinks.style.marginRight = "200px"
+            navLinks.style.width = "200px"
+        }
+
+        var hsla = 0;
+        var bg = document.getElementById('gallerymode');
+        setInterval(()=>{
+        hsla = hsla+1;
+        bg.style.background = `hsla(${hsla},90%, 20%, 1)`;
+        },25);
